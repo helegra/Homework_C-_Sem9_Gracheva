73ab0d0ba5c05a62dff5173854b0e19295871841
helegra <e.b.g@mail.ru>
2022-12-04 17:57:02 +0300

    commit all

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -14,7 +14,7 @@ int FunctionAkkerman(int m, int n)
 {
         if (m == 0)                   //Первое условие
         {
-            return n + 1;
+            return (n + 1);
         } 
         else if (m > 0 && n == 0)    // Второе условие
         {
